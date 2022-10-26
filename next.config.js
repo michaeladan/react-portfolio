1ac1da0b1004ba76cf95c01ed5d2fe3aa45788ca
Michael Adan <electric_boarder@hotmail.com>
2022-10-26 17:21:21 -0600

    add basepath and assetprefix

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  basePath: "/nextjs-pages",
+  assetPrefix: "/nextjs-pages",
 }
 
 const isProd = process.env.NODE_ENV === 'production'
